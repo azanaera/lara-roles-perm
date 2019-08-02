fbc561752a6bb5723e0f3dbd0ffdd4d688fc1f0c
azanaera <azanaera691@gmail.com>
2019-08-02 14:47:44 +0800

    reverted bcrypt

--- app/User.php
+++ app/User.php
@@ -41,6 +41,6 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($password)
     {
-        $this->attributes['password'] = $password;
+        $this->attributes['password'] = bcrypt($password);
     }
 }
